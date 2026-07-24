f252595275967344b804206227464ff347bc7ca4
ElMiope <maxiceisa77@gmail.com>
2026-07-24 17:27:34 -0300

    cree el proyecto

---
+++ src/main/java/com/app/reunite/ReuniteApplication.java
@@ -0,0 +1,13 @@
+package com.app.reunite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReuniteApplication.class, args);
+	}
+
+}
